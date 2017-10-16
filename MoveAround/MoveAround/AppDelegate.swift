6e4d81a1da62306028ff4dbf7b0500616174d38a
Naomi Ngan <naomi.ngan@cbsinteractive.com>
2017-10-15 18:09:09 -0700

    Add Parse

--- MoveAround/MoveAround/AppDelegate.swift
+++ MoveAround/MoveAround/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    // Initialize Parse
+    Parse.initialize(
+        with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+            configuration.applicationId = "movearound"
+            configuration.clientKey = nil  // clientKey is not used on Parse open source unless explicitly configured
+            configuration.server = "https://movearound.herokuapp.com/parse"
+        })
+    )
+    
     // Override point for customization after application launch.
     return true
   }
