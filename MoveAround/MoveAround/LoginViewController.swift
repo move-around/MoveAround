929fe23cf0e1f69989a4b8b6bbb3e837b87840d3
Gonzalo Maldonado <elg0nz@gmail.com>
2017-10-21 17:09:49 -0700

    Set CurrentUser from Facebook Login

--- MoveAround/MoveAround/LoginViewController.swift
+++ MoveAround/MoveAround/LoginViewController.swift
@@ -15,6 +15,7 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
         switch result {
         case .success(grantedPermissions: _, declinedPermissions:  _, token: _):
             print("logged in")
+            self.getUserDetails()
             break;
         default:
             print("Could not login.")
@@ -26,20 +27,52 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
         print("Logged Out")
     }
 
+    func getUserDetails() {
+        let params = ["fields" : "id, first_name, last_name"]
+        let graphRequest = GraphRequest(graphPath: "me", parameters: params)
+        graphRequest.start {
+            (urlResponse, requestResult) in
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
-    loginButton.center = view.center
+            switch requestResult {
+            case .failed(let error):
+                print("error in graph request:", error)
+                break
+            case .success(let graphResponse):
+                if let responseDictionary = graphResponse.dictionaryValue {
+                    if let userDict = responseDictionary as NSDictionary! {
+                        User.currentUser = User(dictionary: userDict)
+                        self.presentSignupVC()
+                    }
+                }
+            }
+        }
+    }
 
-    view.addSubview(loginButton)
-}
+    func presentSignupVC() {
+        print("will present")
+        let signupStoryBoard = UIStoryboard(name: "Signup", bundle: nil)
+        let signupVC = signupStoryBoard.instantiateViewController(withIdentifier: "SignupViewController") as! SignupViewController
+        self.navigationController?.present(signupVC, animated: true, completion: nil)
+    }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        if let current = User.currentUser {
+            self.presentSignupVC()
+        }
+
+        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+        loginButton.center = view.center
+        loginButton.delegate = self
+
+        view.addSubview(loginButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
--- MoveAround/MoveAround/Models/User.swift
+++ MoveAround/MoveAround/Models/User.swift
@@ -18,4 +18,49 @@ class User: NSObject {
     
     var interests: [String]?
     var selectedPlaces: [Place]?
+
+    private var dictionary: NSDictionary?
+
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        if let dictId = dictionary["id"] as? String {
+            id = dictId
+        }
+        if let dictFirstName = dictionary["first_name"] as? String {
+            firstName = dictFirstName
+        }
+        if let dictLastName = dictionary["last_name"] as? String {
+            lastName = dictLastName
+        }
+    }
+
+    static private var _currentUser: User?
+    class var currentUser: User? {
+        get {
+            if (_currentUser != nil) {
+                return _currentUser
+            }
+
+            let data = UserDefaults.standard.object(forKey: "currentUserData") as? Data
+            if (data != nil) {
+                let dictionary = try! JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
+                _currentUser = User(dictionary: dictionary)
+            }
+            return _currentUser
+        }
+        set(user) {
+            _currentUser = user
+            let defaults = UserDefaults.standard
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+            } else {
+                defaults.set(nil, forKey: "currentUserData")
+            }
+            defaults.synchronize()
+        }
+    }
 }
+
+
+
